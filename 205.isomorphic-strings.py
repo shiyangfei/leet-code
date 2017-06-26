d08acf8e8875e189140e171233335ca7f345b0f3
Shiyang Fei <sfei.tek@gmail.com>
2017-06-26 18:08:59 -0400

    #205 completed

---
+++ 205.isomorphic-strings.py
@@ -0,0 +1,35 @@
+#
+# [205] Isomorphic Strings
+#
+# https://leetcode.com/problems/isomorphic-strings
+#
+# Easy (33.50%)
+# Total Accepted:    
+# Total Submissions: 
+# Testcase Example:  '"egg"\n"add"'
+#
+# Can you solve this problem? 🤔
+#
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s_map = dict()
+        t_map = dict()
+        for index, c in enumerate(s):
+            char_s = s[index]
+            char_t = t[index]
+            if char_s not in s_map:
+                s_map[char_s] = char_t
+            else:
+                if char_t != s_map[char_s]:
+                    return False
+            if char_t not in t_map:
+                t_map[char_t] = char_s
+            else:
+                if char_s != t_map[char_t]:
+                    return False
+        return True
